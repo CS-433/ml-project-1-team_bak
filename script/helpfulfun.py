8b0cf896e5660b40b443232bcd25fe1bead31354
bocini <ernesto.bocini@epfl.ch>
2022-10-12 16:00:32 +0200

    added preliminary script

---
+++ script/helpfulfun.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""some helper functions."""
+
+import numpy as np
+
+
+def load_data():
+    """load data."""
+    data = np.genfromtxt("train.csv", dtype=None, delimiter=",", skip_header =1, unpack=True)
+    y = data[1]
+    x = data[2:]
+    return x, y
+
+def compute_error(y,tx,w):
+     return 1 - tx.dot(w)
+
+def compute_loss(y, tx, w):
+
+    """Calculate the loss using either MSE.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+    e = y - tx.dot(w)
+    loss = 1/(2*len(y)) * e.T.dot(e)
+    return(loss)
+
+def compute_loss_mae(y, tx, w):
+    
+    e = y - tx.dot(w)
+    num = 0
+    for i in range(len(y)):
+        num += abs(e[i])
+    return num/len(y)
+
+def compute_gradient(y, tx, w):
+    """Computes the gradient at w.
+        
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2, ). The vector of model parameters.
+        
+    Returns:
+        An numpy array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
+    """
+    e = y - tx.dot(w)
+    return -1/len(y) * tx.T.dot(e)
+
+def compute_stoch_gradient(y, tx, w):
+    """Compute a stochastic gradient at w from just few examples n and their corresponding y_n labels.
+        
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2, ). The vector of model parameters.
+        
+    Returns:
+        A numpy array of shape (2, ) (same shape as w), containing the stochastic gradient of the loss at w.
+    """
+    e = y - tx.dot(w)
+    return -1/len(y) * tx.T.dot(e)
+
+
+
+
+def load_data_from_ex02(sub_sample=True, add_outlier=False):
+    """Load data and convert it to the metric system."""
+    path_dataset = "height_weight_genders.csv"
+    data = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[1, 2])
+    height = data[:, 0]
+    weight = data[:, 1]
+    gender = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[0],
+        converters={0: lambda x: 0 if b"Male" in x else 1})
+    # Convert to metric system
+    height *= 0.025
+    weight *= 0.454
+
+    # sub-sample
+    if sub_sample:
+        height = height[::50]
+        weight = weight[::50]
+
+    if add_outlier:
+        # outlier experiment
+        height = np.concatenate([height, [1.1, 1.2]])
+        weight = np.concatenate([weight, [51.5/0.454, 55.3/0.454]])
+
+    return height, weight, gender
+
+
+def standardize(x):
+    """Standardize the original data set."""
+    mean_x = np.mean(x)
+    x = x - mean_x
+    std_x = np.std(x)
+    x = x / std_x
+    return x, mean_x, std_x
+
+
+def build_model_data(height, weight):
+    """Form (y,tX) to get regression data in matrix form."""
+    y = weight
+    x = height
+    num_samples = len(y)
+    tx = np.c_[np.ones(num_samples), x]
+    return y, tx
