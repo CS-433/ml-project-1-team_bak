a0b65a6f823cf8a943399881dce087a4ab549761
elifKurtay <elif.kurtay00@gmail.com>
2022-10-18 14:41:48 +0200

    clean helper funcs

--- script/helpfulfun.py
+++ script/helpfulfun.py
@@ -3,19 +3,21 @@
 
 import numpy as np
 
-
-def load_data():
-    """load data."""
-    data = np.genfromtxt("train.csv", dtype=None, delimiter=",", skip_header =1, unpack=True)
-    y = data[1]
-    x = data[2:]
-    return x, y
-
+# COMPUTING LOSS 
+#*************************************************************************
 def compute_error(y,tx,w):
-     return 1 - tx.dot(w)
+     return y - tx.dot(w).reshape((y.shape[0],))
+    
+def compute_mse(e):
+    """Calculate the mse for vector e."""
+    return 1/2*np.mean(e**2)
 
+def compute_mae(e):
+    """Calculate the mae for vector e."""
+    return np.abs(e).mean()
+
+# by default MSE
 def compute_loss(y, tx, w):
-
     """Calculate the loss using either MSE.
 
     Args:
@@ -26,18 +28,16 @@ def compute_loss(y, tx, w):
     Returns:
         the value of the loss (a scalar), corresponding to the input parameters w.
     """
-    e = y - tx.dot(w)
-    loss = 1/(2*len(y)) * e.T.dot(e)
-    return(loss)
+    e = compute_error(y, tx, w)
+    return compute_mse(e)
 
 def compute_loss_mae(y, tx, w):
     
-    e = y - tx.dot(w)
-    num = 0
-    for i in range(len(y)):
-        num += abs(e[i])
-    return num/len(y)
+    e = compute_error(y, tx, w)
+    return compute_mae(e)
 
+# COMPUTING GRADIENT 
+#*************************************************************************
 def compute_gradient(y, tx, w):
     """Computes the gradient at w.
         
@@ -49,53 +49,12 @@ def compute_gradient(y, tx, w):
     Returns:
         An numpy array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
     """
-    e = y - tx.dot(w)
-    return -1/len(y) * tx.T.dot(e)
+    e = compute_error(y, tx, w)
+    grad = (-1/y.shape[0]) * (tx.T.dot(e))
+    return grad
 
-def compute_stoch_gradient(y, tx, w):
-    """Compute a stochastic gradient at w from just few examples n and their corresponding y_n labels.
-        
-    Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        w: numpy array of shape=(2, ). The vector of model parameters.
-        
-    Returns:
-        A numpy array of shape (2, ) (same shape as w), containing the stochastic gradient of the loss at w.
-    """
-    e = y - tx.dot(w)
-    return -1/len(y) * tx.T.dot(e)
-
-
-
-
-def load_data_from_ex02(sub_sample=True, add_outlier=False):
-    """Load data and convert it to the metric system."""
-    path_dataset = "height_weight_genders.csv"
-    data = np.genfromtxt(
-        path_dataset, delimiter=",", skip_header=1, usecols=[1, 2])
-    height = data[:, 0]
-    weight = data[:, 1]
-    gender = np.genfromtxt(
-        path_dataset, delimiter=",", skip_header=1, usecols=[0],
-        converters={0: lambda x: 0 if b"Male" in x else 1})
-    # Convert to metric system
-    height *= 0.025
-    weight *= 0.454
-
-    # sub-sample
-    if sub_sample:
-        height = height[::50]
-        weight = weight[::50]
-
-    if add_outlier:
-        # outlier experiment
-        height = np.concatenate([height, [1.1, 1.2]])
-        weight = np.concatenate([weight, [51.5/0.454, 55.3/0.454]])
-
-    return height, weight, gender
-
-
+# Preprocessing
+#*************************************************************************
 def standardize(x):
     """Standardize the original data set."""
     mean_x = np.mean(x)
@@ -104,35 +63,27 @@ def standardize(x):
     x = x / std_x
     return x, mean_x, std_x
 
-
-def build_model_data(height, weight):
-    """Form (y,tX) to get regression data in matrix form."""
-    y = weight
-    x = height
-    num_samples = len(y)
-    tx = np.c_[np.ones(num_samples), x]
-    return y, tx
-
 # LOGISTIC REGRESSION FUNCTIONS
+#*************************************************************************
 def sigmoid(t):
     # sigmoid function is applied on t.
     return 1.0 / (1 + np.exp(-t))
 
 def calculate_loss_lr(y, tx, w):
     # computing the cost by negative log likelihood.
-    pred = sigmoid(tx.dot(w))
+    pred = sigmoid(tx.dot(w).reshape((tx.shape[0],)))
     loss = y.T.dot(np.log(pred)) + (1 - y).T.dot(np.log(1 - pred))
     return np.squeeze(- loss)
 
 def calculate_gradient_lr(y, tx, w):
     """compute the gradient of loss."""
-    pred = sigmoid(tx.dot(w))
+    pred = sigmoid(tx.dot(w).reshape((tx.shape[0],)))
     grad = tx.T.dot(pred - y)
     return grad
 
 def calculate_hessian(y, tx, w):
     """return the Hessian of the loss function."""
-    pred = sigmoid(tx.dot(w))
+    pred = sigmoid(tx.dot(w).reshape((tx.shape[0],)))
     pred = np.diag(pred.T[0])
     r = np.multiply(pred, (1-pred))
     return tx.T.dot(r).dot(tx)
