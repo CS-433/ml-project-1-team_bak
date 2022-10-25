155b0b6a0abb9da067681731da30a112277254fe
aydemirrabdullah <abdullah.aydemir@epfl.ch>
2022-10-25 15:08:01 +0200

    colinearity

--- script/helpfulfun.py
+++ script/helpfulfun.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """some helper functions."""
 
+from cmath import isnan, nan
+from re import A
 import numpy as np
+import copy
 
 # COMPUTING LOSS 
 #*************************************************************************
@@ -185,3 +188,95 @@ def cross_validation(y, x, k_indices, k, lambda_, degree):
     loss_te = np.sqrt(2 * compute_mse(y_test_sub, tx_test, w))
     
     return loss_tr, 
+
+def least_squares(y, tx):
+    """Calculate the least squares solution.
+       returns mse, and optimal weights.
+    
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+    
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar.
+
+    >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
+    (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # least squares: TODO
+    # returns mse, and optimal weights
+    # ***************************************************
+    # Generate the grid of parameters to be swept
+    
+    opt_weights = np.linalg.solve(tx.T.dot(tx),(tx.T.dot(y)))
+
+    e = y - tx.dot(opt_weights)
+    mse = 1/(2*len(y)) * e.T.dot(e)
+    return opt_weights, mse
+
+def get_median(a):
+    a[a == -999.0] = np.nan
+    median=np.nanmedian(a)
+    return median
+
+#def replace_nans_with_median(a,median):
+ #   a[isnan(a)] = median
+    
+    
+def replace_with_median(array_of_features):
+    replaced=copy.deepcopy(array_of_features)
+    for i in range(replaced[0,:].size):
+        #when median is directly computed the value is -999 for columns with alot of missing data
+        #find median while dismissing -999 values
+        median_value=get_median(replaced[:,i])
+        #replace_nans_with_median(array_of_features[:,i],median_value)
+        for j in range(replaced[:,i].size):
+            if np.isnan(replaced[j,i]):
+                replaced[j,i] = median_value
+    return replaced
+
+
+def colinearity_check(array_of_features,critical_value):
+    #regress each feature to all other features
+        #write the code such that it drops same pairs
+    #calculate R^2 value
+    #Use the VIF method 1/(1-R^2) - this will give the colinearity check value
+    #how to find R^2
+    #run machine learning for linear regression
+    #use MSE and use least squares to find the regression
+    #R^2 in terms of MSE and Var(y)
+    #print out the colinearity values of feature pairs
+    features_VIF=np.array(["features x-y","VIF value"])
+    
+    
+    for i in range(array_of_features[0,:].size):
+        y=array_of_features[:,i]
+        #print(y,y.shape)
+        for j in range(i+1,array_of_features[0,:].size):
+            #print(j)
+            num_samples = len(y)
+            tx = np.c_[np.ones(num_samples), array_of_features[:,j]]
+            #print(tx,tx.shape)
+            pt_w,mse=least_squares(y,tx)
+            R_square=1-mse/np.var(y)
+            VIF=1/(1-R_square)
+            if VIF >= critical_value:
+                features_VIF=np.append(features_VIF, [(str(i+1)+"-"+str(j+1)),VIF])
+                
+    #Does this make sense
+    #Or can we just use the panda library and say that these are linear - does it count as being a part of our code!
+    #As it is not excatly within the algorithm
+
+    #TO DO - Feature Selection
+    #variance - covariance - heat plot() -- look at this at first -pairs plot
+    #correlation equation
+    #just use pandas and write in the report as if we didnt use
+    #outliers
+    #unbalanced data - what to do
+    #replace missing with median and outliers with median
+    #spitting data set data
+    #removed old commits - answers
+    return features_VIF
